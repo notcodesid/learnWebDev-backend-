b602428f42f437e46da684fde62e9755396d0ecc
siddharth <notcodesid@gmail.com>
2024-12-28 06:24:45 -0500

    insert data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import express from 'express';
 const app = express ();
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 app.get('/' , (req ,res ) => {
     res.send ('Hello World');
 })
+
+async function insertData(email: string) {
+    const res = await prisma.user.create({
+        data : {
+            email: email
+        }
+    })
+    console.log(res);
+}
+
+insertData("notcodesid@gmail.com")
 
 app.listen(8080 , () => {
     console.log('Server is running on port 8080');
